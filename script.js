4d43c767a4a278d552577c4d3d6b0d3e3e9e2af9
shivani2224 <patelshivani1410@gmail.com>
2024-06-12 02:46:32 -0400

    added cancel button and fucntion on li

--- script.js
+++ script.js
@@ -3,19 +3,31 @@ const taskList = document.getElementById("task-list");
 
 const addItemHandler = () => {
   const userInput = document.getElementById("input").value;
+
   if (userInput.trim() !== "") {
     const outputElement = document.createElement("li");
+    const cancelbtn = document.createElement("button");
+    const textOnButton = document.createTextNode(" x ");
+    cancelbtn.appendChild(textOnButton);
+    cancelbtn.id = "cancel-id";
     outputElement.textContent = userInput;
+
+    console.log(cancelbtn);
 
     outputElement.addEventListener("click", () => {
       if (outputElement.style.textDecoration === "line-through") {
         outputElement.style.textDecoration = "none";
       } else {
         outputElement.style.textDecoration = "line-through";
+        outputElement.style.classList.toggle("checked");
       }
     });
     taskList.appendChild(outputElement);
+    outputElement.appendChild(cancelbtn);
     console.log(outputElement);
+    cancelbtn.addEventListener("click", () => {
+      taskList.removeChild(outputElement);
+    });
     document.getElementById("input").value = "";
   } else {
     alert("Please enter text in task list");
